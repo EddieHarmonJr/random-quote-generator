a7fca2f1d0035e255f7710c7757f437f6f4eecff
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2020-09-16 15:22:24 -0400

    Remove unnecessary hyphens from author attribution.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -11,11 +11,11 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 let quotes = [
-  {quote: "Hey. Don't ever let somebody tell you you can't do something. Not even me. All right? You got a dream... You gotta protect it. People can't do somethin' themselves, they wanna tell you you can't do it. If you want somethin', go get it. Period.", source: "-Chris Gardner:", citation: "Pursuit of Happyness", year: 2006},
-  {quote: "Get busy living or get busy dying.", source: "-Andy Dufresne", citation: "Shawshank Redemption", year: 1994},
-  {quote: "I think it pisses God off when you walk by the color purple in a field and don't notice it.", source: "-Shug Avery", citation: "The Color Purple", year: 1985},
-  {quote: "Let me tell you something you already know. The world ain't all sunshine and rainbows. It's a very mean and nasty place and I don't care how tough you are it will beat you to your knees and keep you there permanently if you let it. You, me, or nobody is gonna hit as hard as life. But it ain't about how hard ya hit. It's about how hard you can get hit and keep moving forward. How much you can take and keep moving forward. That's how winning is done!", source: "-Rocky Balboa", citation: "Rocky V", year: 1990},
-  {quote: "Bury me in the ocean, with my ancestors that jumped from the ships, because they knew death was better than bondage.", source: "-Killmonger", citation: "Black Panther", year: 2018 },
+  {quote: "Hey. Don't ever let somebody tell you you can't do something. Not even me. All right? You got a dream... You gotta protect it. People can't do somethin' themselves, they wanna tell you you can't do it. If you want somethin', go get it. Period.", source: "Chris Gardner:", citation: "Pursuit of Happyness", year: 2006},
+  {quote: "Get busy living or get busy dying.", source: "Andy Dufresne", citation: "Shawshank Redemption", year: 1994},
+  {quote: "I think it pisses God off when you walk by the color purple in a field and don't notice it.", source: "Shug Avery", citation: "The Color Purple", year: 1985},
+  {quote: "Let me tell you something you already know. The world ain't all sunshine and rainbows. It's a very mean and nasty place and I don't care how tough you are it will beat you to your knees and keep you there permanently if you let it. You, me, or nobody is gonna hit as hard as life. But it ain't about how hard ya hit. It's about how hard you can get hit and keep moving forward. How much you can take and keep moving forward. That's how winning is done!", source: "Rocky Balboa", citation: "Rocky V", year: 1990},
+  {quote: "Bury me in the ocean, with my ancestors that jumped from the ships, because they knew death was better than bondage.", source: "Killmonger", citation: "Black Panther", year: 2018 },
 ];
 
 // console.log(quotes);
